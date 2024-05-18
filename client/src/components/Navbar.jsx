4fe6072a8dcbe4b07d8ab741fca78f8d5019295f
zabir emu <zabirraihan570@gmail.com>
2024-05-18 14:59:27 +0600

    menu completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -95,9 +95,64 @@ const Navbar = () => {
         <div className="hidden navbar-center lg:flex">
           <ul className="px-1 menu menu-horizontal">{navItems}</ul>
         </div>
-        <div className="navbar-end">
+        <div className="flex items-start navbar-end">
+          <button className="hidden btn btn-ghost btn-circle lg:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <div className="hidden dropdown dropdown-end me-2 lg:block">
+            <div
+              tabindex="0"
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">8</span>
+              </div>
+            </div>
+            <div
+              tabindex="0"
+              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="text-lg font-bold">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <a className="flex items-center gap-2 px-6 text-white rounded-full btn bg-green hover:bg-white hover:text-[#39DB4A] hover:border-[#39DB4A]">
-            <FiPhoneCall />
+            <FiPhoneCall /> Contact
           </a>
         </div>
       </div>
